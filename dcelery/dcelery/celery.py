35967644402bc30bb14094c898f2b1194a23ed9e
runyud <runyud@umich.edu>
2023-11-12 23:17:21 -0800

    set up task queue prioritization for redis

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -1,8 +1,18 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcelery.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dcelery.settings")
 app = Celery("dcelery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.conf.task_routes = {'newapp.tasks.task1' : {'queue':'queue1'}, 'newapp.tasks.task2' : {'queue':'queue2'}}
+# app.conf.task_routes = {
+#     "newapp.tasks.task1": {"queue": "queue1"},
+#     "newapp.tasks.task2": {"queue": "queue2"},
+# }
+
+app.conf.broker_transport_options = {
+    "priority_steps": list(range(10)),
+    "step": ":",
+    "queue_order_strategy": "priority",
+}
+
 app.autodiscover_tasks()
